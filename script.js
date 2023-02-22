3742323c1bc4c5ff6b5bbc86ccfa282742c0cf48
ljimshitashvili <lasha.jimshitashvili@iset.ge>
2023-02-22 19:50:25 +0400

    js experiment

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-$(document).ready(function() {
-    $('#price').on('keyup', function() {
-      var val = $(this).val();
-      if (val != '' && !isNaN(val)) {
-        $(this).val('$' + val);
-      }
-    });
-  });
+alert("Lasha's first JavaScript project")
